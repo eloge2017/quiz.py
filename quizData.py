6323221e806577b86036788f1a0cdeabd5e8faa3
csPython <cspython@AD25TF04NGF1J-95.local>
2019-01-18 10:39:48 -0500

    data files added

---
+++ quizData.py
@@ -0,0 +1,103 @@
+#variables for quizEN.py
+
+score= int(0)
+q1 = """How many toes do you have on both feet?
+(1) 7
+(2) 5
+(3) 9
+(4) 10"""
+
+a1 = int(0)
+check1 = bool(False)
+q1Ans = 4
+
+q2 = """How many fingers do you have on one hand?
+(1) 7
+(2) 5
+(3) 9
+(4) 2"""
+
+a2 = int(0)
+check2 = bool(False)
+q2Ans = 2
+
+q3 = """How many arms do you have?
+(1) 7
+(2) 5
+(3) 9
+(4) 2"""
+
+a3 = int(0)
+check3 = bool(False)
+q3Ans = 4
+
+q4 = """How many colors are there in the rainbow?
+(1) 7
+(2) 5
+(3) 9
+(4) 2"""
+
+a4 = int(0)
+check4 = bool(False)
+q4Ans = 1
+
+q5 = """How many weekdays are in a week?
+(1) 7
+(2) 5
+(3) 9
+(4) 2"""
+
+a5 = int(0)
+check5 = bool(False)
+q5Ans = 2
+
+q6 = """How many weekends are in a week?
+(1) 7
+(2) 5
+(3) 9
+(4) 2"""
+
+a6 = int(0)
+check6 = bool(False)
+q6Ans = 4
+
+q7 = """How tall is Eloge?
+(1) 7
+(2) 6.1
+(3) 10
+(4) 8"""
+
+a7 = int(0)
+check7 = bool(False)
+q7Ans = 2
+
+q8 = """what is eloge's favorite color?
+(1) purple
+(2) blue
+(3) red
+(4) yellow"""
+
+a8 = int(0)
+check8 = bool(False)
+q8Ans = 1
+
+q9 = """what is the number of questions in this quiz?
+(1) 5
+(2) 8
+(3) 10
+(4) 12"""
+
+a9 = int(0)
+check9 = bool(False)
+q9Ans = 3
+
+q10 = """how many stars in the flag?
+(1) 25
+(2) 51
+(3) 50
+(4) 2"""
+
+a10 = int(0)
+check10 = bool(False)
+q10Ans = 3
+
